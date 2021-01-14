4bf1e0065622047e863a373dfeaeff8513d96027
Umair Shahid <umair.shahid0088@gmail.com>
2021-01-14 21:57:02 +0500

    Updates

---
+++ precache-manifest.443627dc4b43a900c60436e8c9a8340d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356871b6565dec17b8a215279a250ac3",
+    "url": "/Expense-tracker-app-in-Reactjs/index.html"
+  },
+  {
+    "revision": "2b10a402077bf881b49a",
+    "url": "/Expense-tracker-app-in-Reactjs/static/css/main.d9ca3109.chunk.css"
+  },
+  {
+    "revision": "d274357ae37698f69cd8",
+    "url": "/Expense-tracker-app-in-Reactjs/static/js/2.87902a96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-tracker-app-in-Reactjs/static/js/2.87902a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b10a402077bf881b49a",
+    "url": "/Expense-tracker-app-in-Reactjs/static/js/main.ed34c0d1.chunk.js"
+  },
+  {
+    "revision": "6243775c0e7a9f3436d8",
+    "url": "/Expense-tracker-app-in-Reactjs/static/js/runtime-main.77490fff.js"
+  }
+]);
